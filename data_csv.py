92ff8c8a49e83a282367f7c5520c78147ae67fe2
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2021-09-29 17:53:18 +0200

    separate csv modul

--- data_csv.py
+++ data_csv.py
@@ -33,3 +33,4 @@ class fileCsv():
 
 
 
+#class preProcesData():
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import data_csv
 
-
-
 if __name__ == "main":
-
     path = "ACUData.csv"
     file = data_csv.fileCsv(path)
     print(file.header)
+    print(file.rows)
+
 
 
 
