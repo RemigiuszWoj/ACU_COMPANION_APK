6b9582b5f45681ff130284f52709b1613d2b772e
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2021-09-29 17:37:06 +0200

    data_csv

---
+++ data_csv.py
@@ -0,0 +1,35 @@
+import csv
+
+class fileCsv():
+    "csv file modifi class"
+    def __init__(self, path:str) -> None:
+        self.path = path
+        self.read()
+
+    def read(self) -> None:
+        self.rows = []
+        self.header =[]
+        with open(self.path, "r") as file:
+            csvreader = csv.reader(file)
+            self.header = next(csvreader) 
+            for row in csvreader:
+                self.rows.append(row)
+
+    def modifi(self, data:list, position="end") -> None:
+        with open(self.path, "a") as file:
+            if position == "end":
+                for row in data:
+                    for x in row:
+                        file.write(str(x)+"; ")
+                    file.write("\n")
+
+    def write(self, data:list, position="end") -> None:
+        with open(self.path, "") as file:
+            if position == "end":
+                for row in data:
+                    for x in row:
+                        file.write(str(x)+"; ")
+                    file.write("\n")
+
+
+
--- main.py
+++ main.py
@@ -1,54 +1,13 @@
-import csv
+import data_csv
 
-class fileCsv():
-    "csv file modifi class"
-    def __init__(self, path):
-        self.path = path
 
-    def read(self):
-        self.rows = []
-        self.header =[]
-        with open(self.path, "r") as file:
-            csvreader = csv.reader(file)
-            self.header = next(csvreader) 
-            for row in csvreader:
-                self.rows.append(row)
 
-    def write(self, data, position="end"):
-        with open(self.path, "w") as file:
-            if position == "end":
-                for row in data:
-                    for x in row:
-                        file.write(str(x),+" ")
-                    file.write("n")
+if __name__ == "main":
+
+    path = "ACUData.csv"
+    file = data_csv.fileCsv(path)
+    print(file.header)
 
 
 
 
-
-
-
-
-path = "ACUData.csv"
-file = fileCsv(path)
-file.read()
-print(file.header)
-print(file.rows)
-
-
-# data = [1,2,3,4]
-# file.write(data)
-# print(file.header)
-# print(file.rows)
-
-
-
-# rows = []
-# patch = "ACUData.csv"
-# with open(patch, "r") as file:
-#     csvreader = csv.reader(file)
-#     header = next(csvreader)
-#     for row in csvreader:
-#         rows.append(row)
-# print(header)
-# print(rows)
